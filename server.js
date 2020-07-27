2c59b4473b6acda118ec02a6e4bf80e3e389ece2
ps1437 <praveen369soni@gmail.com>
2020-07-27 19:16:57 +0530

    chat Room UI added

--- server.js
+++ server.js
@@ -3,26 +3,63 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
-app.get('/', function(req, res) {
+const PORT = process.env.PORT || 8888;
+var userList = {};
+const rooms ={
+ Comedy:{},
+ TimePass:{},
+ Education:{},
+ 
+ 
+
+};
+
+
+app.set('View Engine', '/views');
+app.post('/join', function(req, res) {
+    rooms[req.body.roomId] ={users:{}}
+     res.redirect(req.body.roomId);
+});
+
+app.get('/:roomId', function(req, res) {
+    res.render('Chat.ejs',{roomId:req.params.roomId});
+});
+
+app.get('/room', function(req, res) {
     res.render('Chat.ejs');
 });
+
+app.get('/', function(req, res) {
+    res.render('Join.ejs' ,{rooms:rooms});
+});
+
 
 io.sockets.on('connection', function(socket) {
     socket.on('username', function(username) {
         socket.username = username;
-        io.emit('is_online',  username +" Join the Chat");
+        // if(!userList[username.toUpperCase()]){
+        //     userList[username.toUpperCase()] ="A";
+        // }
+        io.emit('is_online',  username +" Join the Chat",userList);
+    
     });
 
     socket.on('disconnect', function(username) {
-        io.emit('is_online', username +" Left the Chat");
+         delete userList.username;
+        io.emit('is_online', username +" Left the Chat",userList);
     })
+
+    socket.on('typing', function() {
+    
+       io.emit('chat_message', socket.username +" is Typing ",socket.username);
+   })
 
     socket.on('chat_message', function(message) {
         io.emit('chat_message',  message,socket.username);
     });
 
 });
-const PORT = process.env.PORT || 8888;
-const server = http.listen(PORT, function() {
+
+http.listen(PORT, function() {
     console.log(`listening on *:${PORT}`);
 });
